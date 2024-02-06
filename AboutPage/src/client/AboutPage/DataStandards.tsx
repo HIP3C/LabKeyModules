1a1539c5590ba3f096ad3bc66ddbffe2c61bff37
J Greenbaum <jgbaum@gmail.com>
2024-02-05 16:38:48 -0800

    reusing about page css in data standards page

--- AboutPage/src/client/AboutPage/DataStandards.tsx
+++ AboutPage/src/client/AboutPage/DataStandards.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export const DataStandards: React.FC = () => {
   return (
     <div id="DataStandards-content">
-      <section className="DataStandards-standards">
+      <section className="About-content-primary">
       <div className="About-content-text">
           <h2>HIPC Data Standards</h2>
           Please visit <a href="https://immunespace.org/hipc-data-standards">immunespace.org/hipc-data-standards</a> for more information
